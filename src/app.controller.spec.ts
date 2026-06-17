32b8d61a4576a0bf964451e5332e5d26fe3a6254
Ayanabilothman <aya.nabil.othman@gmail.com>
2026-06-17 22:03:39 +0300

    inital commit

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [
+        {
+          provide: AppService,
+          useValue: {
+            getUsers: jest.fn().mockResolvedValue({
+              message: 'Users all from DB!',
+              users: [{ name: 'Aya' }, { name: 'Ahmed' }],
+            }),
+          },
+        },
+      ],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  it('should return all users from DB', async () => {
+    const result = await appController.getUsers();
+
+    expect(result.message).toBe('Users all from DB!');
+    expect(Array.isArray(result.users)).toBe(true);
+  });
+});
---
+++ src/app.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Injectable()
+export class AppService {
+  constructor(
+    @InjectConnection()
+    private readonly connection: Connection,
+  ) {}
+
+  async getUsers() {
+    const users = await this.connection.collection('users').find({}).toArray();
+
+    return {
+      message: 'Users all from DB!',
+      users,
+    };
+  }
+
+  async createUser() {
+    const result = await this.connection
+      .collection('users')
+      .insertOne({ name: 'Ali Mohamed', age: 20 });
+
+    return {
+      message: 'User inserted successfully!',
+      result,
+    };
+  }
+}
